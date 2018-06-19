1c7e10996941f05a27132209532f7016ee2e32ee
Shubham Kumar <shubham.kumar1739@gmail.com>
2018-06-20 03:00:30 +0530

    Add login with manually created user

--- app/src/main/java/chat/chat/chat/MainActivity.java
+++ app/src/main/java/chat/chat/chat/MainActivity.java
@@ -2,26 +2,71 @@ package chat.chat.chat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import chat.chat.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private EditText user;
+    private EditText password;
+    private Button logIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        startActivity(new Intent(MainActivity.this,ChatActivity.class));
-        finish();
+        mAuth=FirebaseAuth.getInstance();
+        user=(EditText)findViewById(R.id.user);
+        password=(EditText)findViewById(R.id.password);
+        logIn=(Button)findViewById(R.id.logIn);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name,pass;
+                name=user.getText().toString();
+                pass=password.getText().toString();
+                if(TextUtils.isEmpty(name)||TextUtils.isEmpty(pass))
+                {
+                    Toast.makeText(getApplicationContext(),"Empty Field!",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    name=name+"@abc.com";
+                    mAuth.signInWithEmailAndPassword(name,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                startActivity(new Intent(MainActivity.this, ChatActivity.class));
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(), "Authentication failed.",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
     }
 
     @Override
