f02cb2856d1d2a9d6edf7268d1188db2bc3d8f3a
Shubham Kumar <shubham.kumar1739@gmail.com>
2018-06-20 00:16:43 +0530

    Complete Stable UI for Chat With Person

---
+++ app/src/main/java/chat/chat/chat/ChatActivity.java
@@ -0,0 +1,24 @@
+package chat.chat.chat;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import chat.chat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
--- app/src/main/java/chat/chat/chat/MainActivity.java
+++ app/src/main/java/chat/chat/chat/MainActivity.java
@@ -1,5 +1,6 @@
 package chat.chat.chat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -28,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        startActivity(new Intent(MainActivity.this,ChatActivity.class));
+        finish();
     }
 
     @Override
